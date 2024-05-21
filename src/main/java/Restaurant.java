085b2bad7f7dac0eede1a782b3cc238c539c55aa
RishiRajrr2016 <rishiraj.rr2016@gmail.com>
2024-05-21 15:16:04 +0000

    Part 2: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -1,10 +1,9 @@
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Restaurant {
-    private String name;
+	private String name;
     private String location;
     public LocalTime openingTime;
     public LocalTime closingTime;
@@ -18,15 +17,16 @@ public class Restaurant {
     }
 
     public boolean isRestaurantOpen() {
-        return true;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+    	LocalTime currentTime = this.getCurrentTime();
+    	if((currentTime.compareTo(openingTime)>=0) && (currentTime.compareTo(closingTime)<0))
+    		return true;
+    	else return false;
     }
 
     public LocalTime getCurrentTime(){ return  LocalTime.now(); }
 
     public List<Item> getMenu() {
-        return null;
-        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+        return menu;
     }
 
     private Item findItemByName(String itemName){
